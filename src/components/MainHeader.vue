4d0ed51493da56e94ad6680899e7ee754a92d5e3
DanieleTrippodo <trippododaniele@gmail.com>
2024-06-10 15:02:25 +0200

    fixed html

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -10,21 +10,24 @@ export default {
 <!-- Qui va il contenuto di questo elemento (HTML) -->
 <template>
   <header>
+    <!-- Logo DC -->
     <div class="logo">
       <img src="/src/assets/img/dc-logo.png" alt="DC Logo">
     </div>
+
+    <!-- lista link -->
     <nav>
       <ul>
-        <li><a href="#">CHARACTERS</a></li>
-        <li><a href="#">COMICS</a></li>
-        <li><a href="#">MOVIES</a></li>
-        <li><a href="#">TV</a></li>
-        <li><a href="#">GAMES</a></li>
-        <li><a href="#">COLLECTIBLES</a></li>
-        <li><a href="#">VIDEOS</a></li>
-        <li><a href="#">FANS</a></li>
-        <li><a href="#">NEWS</a></li>
-        <li><a href="#">SHOP</a></li>
+        <li>CHARACTERS</li>
+        <li>COMICS</li>
+        <li>MOVIES</li>
+        <li>TV</li>
+        <li>GAMES</li>
+        <li>COLLECTIBLES</li>
+        <li>VIDEOS</li>
+        <li>FANS</li>
+        <li>NEWS</li>
+        <li>SHOP</li>
       </ul>
     </nav>
   </header>
